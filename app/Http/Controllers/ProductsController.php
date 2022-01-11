bb9a1568202ed0f76411439b681707238842c011
CrestNiraj12 <crestniraj@gmail.com>
2022-01-12 02:30:08 +0545

    added sort

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     public function index() {
-        $products = Product::with("category")->paginate(9);
+        $products = Product::with("category")->orderBy("title")->paginate(9);
         $productCategories = ProductCategory::all();
         return view("pages.products", ["paginator" => $products, "categories" => $productCategories]);
     }
@@ -20,7 +20,18 @@ class ProductsController extends Controller
             $products = $products->whereHas("category", function ($q) use ($request) {
                 $q->where("title", $request->cat);
             });
+
+        if ($request->sort == "RECENT")
+            $products->orderBy("updated_at", "desc");
+        elseif ($request->sort == "PRICE_ASC")
+            $products->orderBy("price");
+        elseif ($request->sort == "PRICE_DESC")
+            $products->orderBy("price", "desc");
+        elseif ($request->sort == "DESC")
+            $products->orderBy("title", "desc");
+        else $products->orderBy("title");
+
         $productCategories = ProductCategory::all();
-        return view("pages.products", ["paginator" => $products->paginate(9), "categories" => $productCategories, "search_query" => $request->search_query]);
+        return view("pages.products", ["paginator" => $products->paginate(9), "categories" => $productCategories, "search_query" => $request->search_query, "sort" => $request->sort]);
     }
 }
--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -16,16 +16,29 @@
       <div class="row">
         <div class="col-md-12">
           <div class="filters">
+              <div class="select-group form-select custom-search-form">
+                <form id="sortForm" method="GET" action="{{ route("search") }}">
+                  <input type="hidden" name="cat" id="catValue" />
+                  <input type="hidden" name="search_query" id="search_query" />
+                  <select id="sort" name="sort" class="form-select" value="{{ $sort ?? "" }}">
+                    <option value="ASC" {{ isset($sort) ? ($sort == "ASC" ? "selected" : "") : ""  }}>Ascending</option>
+                    <option value="DESC" {{ isset($sort) ? ($sort == "DESC" ? "selected" : "") : ""  }}>Descending</option>
+                    <option value="PRICE_ASC" {{ isset($sort) ? ($sort == "PRICE_ASC" ? "selected" : "") : ""  }}>Low to High Price</option>
+                    <option value="PRICE_DESC" {{ isset($sort) ? ($sort == "PRICE_DESC" ? "selected" : "") : ""  }}>High to Low Price</option>
+                    <option value="RECENT" {{ isset($sort) ? ($sort == "RECENT" ? "selected" : "") : ""  }}>Recent</option>
+                  </select>
+                </form>
+              </div>
                 <ul>
-                    <li class="{{ (str_contains(Request::fullUrl(), "cat=All") or Request::is('products')) ? "active" : "" }}" data-filter="All"><a href="{{ "/products/search?cat=All&search_query=" . ($search_query ?? "") }}">All Products</a></li>
+                    <li class="{{ (str_contains(Request::fullUrl(), "cat=All") or Request::is('products')) ? "active" : "" }}" data-value="All"><a href="{{ "/products/search?cat=All&search_query=" . ($search_query ?? "") }}">All Products</a></li>
                     @foreach ($categories as $category)
-                        <li class="{{ (str_contains(Request::fullUrl(), "cat=".$category->title)) ? "active" : "" }}" data-filter="{{ $category->title }}"><a href="{{ "/products/search?cat=" . $category->title . "&search_query=" . ($search_query ?? "")}}">{{ $category->title }}s</a></li>
+                        <li class="{{ (str_contains(Request::fullUrl(), "cat=".$category->title)) ? "active" : "" }}" data-value="{{ $category->title }}"><a href="{{ "/products/search?cat=" . $category->title . "&search_query=" . ($search_query ?? "")}}">{{ $category->title }}s</a></li>
                     @endforeach
                 </ul>
                 <form id="searchForm" method="GET" action="{{ route("search") }}">
                     <input type="hidden" name="cat" id="cat" />
                     <div class="input-group custom-search-form">
-                        <input type="text" class="form-control" value="{{ $search_query ?? "" }}" name="search_query" placeholder="Search...">
+                        <input id="search_query" type="text" class="form-control" value="{{ $search_query ?? "" }}" name="search_query" placeholder="Search...">
                         <span class="input-group-btn">
                             <button class="btn btn-default-sm" type="submit" id="search">
                                 <i class="fa fa-search"></i>
@@ -72,12 +85,22 @@
   <script type="text/javascript">
     window.onload = function () {
         $(document).on('click', "#search", search);
+        $(document).on('change', "#sort", sort);
 
         function search(e) {
             e.preventDefault();
-            var activeCatFilter = $('.products .filters li[class="active"]').attr('data-filter');
+            var activeCatFilter = $('.products .filters li[class="active"]').attr('data-value');
             $('#cat').val(activeCatFilter);
             $("#searchForm").submit();
+        }
+
+        function sort(e) {
+            e.preventDefault();
+            var activeCatFilter = $('.products .filters li[class="active"]').attr('data-value');
+            var searchQuery = $('#searchForm #search_query').val();
+            $('#catValue').val(activeCatFilter);
+            $("#search_query").val(searchQuery);
+            $("#sortForm").submit();
         }
     }
 
